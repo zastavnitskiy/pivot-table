f510ccdd39be2b89a89b36f5b3f6a12a82ec63a3
Pavel Zastavnitskiy <pasha.zastavnitskiy@gmail.com>
2019-08-18 17:36:33 +0200

    Styling

--- src/components/PivotTable/components/Header/Header.tsx
+++ src/components/PivotTable/components/Header/Header.tsx
@@ -5,6 +5,6 @@ import styles from "./Header.module.css";
 export const Header: React.FC<TableProps> = props => (
   <div className={styles.container}>
     <h1 className={styles.header}>{props.tableName}</h1>
-    <div className={styles.inputs}>Density</div>
+    <div className={styles.inputs}>Density(to be implemnted)</div>
   </div>
 );
